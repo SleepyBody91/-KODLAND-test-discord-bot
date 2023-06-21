0df742cca3a9110371d1114ca9ba9fca874d3e02
SleepyBody91 <89473999+SleepyBody91@users.noreply.github.com>
2023-06-21 19:07:13 +0300

    discord.py

--- discord.py
+++ discord.py
@@ -1,13 +1,28 @@
 import discord
 from discord.ext import commands
-import openai #ai
+import openai
+import requests
+import os
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-openai.api_key = 'КЛЮЧ_OpenAi' # тут ключ Open AI
+openai.api_key = 'api' # тут ключ Open AI
+
+def get_dog_image_url():
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command(name='собака')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 @bot.event
 async def on_ready():
@@ -18,7 +33,7 @@ async def hello(ctx):
     await ctx.send(f'Привет! Я бот {bot.user}!')
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
+async def heh(ctx, count_heh=5):
     await ctx.send("he" * count_heh)
 
 @bot.command()
@@ -29,11 +44,15 @@ async def add(ctx, left: int, right: int):
 @bot.command()
 async def ask(ctx, *, question):
     response = openai.Completion.create(
-        engine='davinci', #движок
+        engine='davinci', # движок
         prompt=question, # тип вопроса
-        max_tokens=50 #ограничение в токинах
+        max_tokens=50 # ограничение в токенах
     )
-    await ctx.send(response.choices[0].text) #отправка ответа
+    await ctx.send(response.choices[0].text) # отправка ответа
+
+@bot.command()
+async def e_help(ctx):
+    await ctx.send("команды сервера префикс бота = (command_prefix),команды: ask - отправить запрос ИИ (токин не дам, add - добавляет a + b = c)")
 
 @bot.group()
 async def cool(ctx):
@@ -45,9 +64,15 @@ async def cool(ctx):
         await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
 
 @bot.command()
-async def help(ctx):
-   # """Выводит список комманд."""
-    command_list = [command.name for command in bot.commands]
-    await ctx.send(f'Available commands: {", ".join(command_list)}')
+async def mem(ctx):
+    random_mem = random.randint(1, 2)
+    if random_mem == 1:
+        with open('images/mem1.jpg', 'rb') as f:
+            picture = discord.File(f)
+            await ctx.send(file=picture)
+    elif random_mem == 2:
+        with open('images/mem2.jpg', 'rb') as f:
+            picture = discord.File(f)
+            await ctx.send(file=picture)
 
-bot.run('Ключ_бота_дискорд')
+bot.run('api_key')
